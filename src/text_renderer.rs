3f2691339601a94a279163bd8378c6e5eff285b9
kaileh57 <kellenhe@hotmail.com>
2025-05-22 10:29:39 -0700

    .

--- src/text_renderer.rs
+++
@@ -1,4 +0,0 @@
-// text_renderer.rs - Re-export TextRenderer from ui/text_renderer module
-// This is a convenience module to provide access to text rendering functionality
-
-pub use crate::ui::text_renderer::*; 
--- src/ui_components.rs
+++
@@ -1,4 +0,0 @@
-// ui_components.rs - Re-export UI components from ui/components module
-// This is a convenience module to provide access to UI components
-
-pub use crate::ui::components::*; 
